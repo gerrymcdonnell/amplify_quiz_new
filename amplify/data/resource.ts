12494838db01e1b8f3e63be51f5d4c31cbfe8b40
Gerry Mc Donnell <gerrymcdonnell@users.noreply.github.com>
2026-02-08 23:54:22 +0000

    updated app.tsx and data/resource.ts

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,6 +1,30 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
 const schema = a.schema({
+  generateQuestions: a
+    .generation({
+      aiModel: a.ai.model("Claude 3.5 Sonnet"),
+      systemPrompt: `
+You are a quiz question generator.
+
+Create exactly 10 questions, evenly distributed across the categories from the following list [Sport, General Culture, Movies, Art, History]. Ensure the questions are evenly distributed in different difficulty levels.
+
+Requirements for each question:
+- The questions should be in English.
+- Return the result as a JSON list containing JSON objects.
+- Return the question with the JSON key 'question'.
+- Include 4 different answer options, with the JSON key 'options', each a string.
+- Specify 1 correct answer, with the JSON key 'correctAnswer', in string format.
+- Return the category with the JSON key 'category'.
+- The returned JSON will only have keys and values from the information from the mentioned before. Do not add any explanatory messages or statements such as 'Here is a JSON containing your trip', so user can take the JSON string and play around with it.
+- Questions should not be repeated.
+    `,
+    })
+    .arguments({
+      description: a.string(),
+    })
+    .returns(a.ref("Question").required().array().required())
+    .authorization((allow) => allow.authenticated()),
   Question: a.customType({
     question: a.string().required(),
     options: a.string().required().array().required(),
